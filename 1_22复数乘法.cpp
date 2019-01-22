6d6e022c17ab6a3fde4aea69cf90c54688a0458b
liyinghui <li-yh16@mails.tsinghua.edu.cn>
2019-01-22 09:44:44 +0800

    upload 1/22

---
+++ 1_22复数乘法.cpp
@@ -0,0 +1,20 @@
+/*
+给定两个表示复数的字符串。
+
+返回表示它们乘积的字符串。注意，根据定义 i2 = -1 。
+
+*/
+
+public class Solution {
+
+    public String complexNumberMultiply(String a, String b) {
+        String x[] = a.split("\\+|i");
+        String y[] = b.split("\\+|i");
+        int a_real = Integer.parseInt(x[0]);
+        int a_img = Integer.parseInt(x[1]);
+        int b_real = Integer.parseInt(y[0]);
+        int b_img = Integer.parseInt(y[1]);
+        return (a_real * b_real - a_img * b_img) + "+" + (a_real * b_img + a_img * b_real) + "i";
+
+    }
+}
